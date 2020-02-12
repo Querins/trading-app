39930fcaff9167b856454e5ac0152e9e7d4b6ec0
Alex Querins <querins777@gmail.com>
2020-02-13 00:48:55 +0200

    First draft of clients table

--- src/App.js
+++ src/App.js
@@ -3,9 +3,36 @@ import './App.css';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { clients: [] }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/users")
+    .then(res => res.json())
+    .then(json => this.setState({clients: json}));
+  }
+
   render() {
     return (
-      
+      <table>
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+        </tr>
+        </thead>
+        <tbody>
+          { this.state.clients.map(client => (
+          <tr>
+            <td>{client.id}</td>
+            <td>{client.name}</td>
+          </tr>
+        ) ) }
+        </tbody>
+
+      </table>
     );
   }
 
